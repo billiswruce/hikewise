a765ae6dbfc7410838933ae8fa301f9f184e8912
billiswruce <jessicatell-@hotmail.com>
2024-10-30 22:44:06 +0100

    Mongodb atlas installed

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
 import "colors";
+import cors from "cors";
+import mongoose from "mongoose";
 
 dotenv.config();
+
+const dbURI = process.env.MONGO_URI;
+
+mongoose
+  .connect(dbURI)
+  .then(() => {
+    console.log("MongoDB Connected Successfully".green);
+  })
+  .catch((err) => {
+    console.error("MongoDB Connection Failed:", err.red);
+  });
+
 const app = express();
 const PORT = process.env.PORT || 6000;
-// app.get("/", (req, res) => {
-//   res.send("Server is running");
-// });
 
-app.listen(PORT, () => console.log("🚀 Server is blooming".rainbow.bold));
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(PORT, () => console.log("Server is blooming".rainbow.bold));
