647a31fa2192cf5a5cd338ccfc3590eda8e526ca
Jessica Tell <jessicatell-@hotmail.com>
2024-12-05 16:46:55 +0100

    Font size globals

--- client/src/components/Filter.tsx
+++ client/src/components/Filter.tsx
@@ -1,5 +1,6 @@
 import { Dispatch, SetStateAction } from "react";
 import styles from "../styles/Hiking.module.scss";
+import { useTranslation } from "react-i18next";
 
 type SortOption = "name-asc" | "name-desc" | "date-asc" | "date-desc";
 
@@ -9,17 +10,19 @@ interface FilterProps {
 }
 
 const Filter = ({ sortOption, setSortOption }: FilterProps) => {
+  const { t } = useTranslation();
+
   return (
     <div className={styles.filterContainer}>
-      <label htmlFor="sort-options">Sortera efter:</label>
+      <label htmlFor="sort-options">{t("filter.sortLabel")}</label>
       <select
         id="sort-options"
         value={sortOption}
         onChange={(e) => setSortOption(e.target.value as SortOption)}>
-        <option value="name-asc">Namn (A-Ö)</option>
-        <option value="name-desc">Namn (Ö-A)</option>
-        <option value="date-asc">Datum (Äldsta först)</option>
-        <option value="date-desc">Datum (Nyaste först)</option>
+        <option value="name-asc">{t("filter.options.nameAsc")}</option>
+        <option value="name-desc">{t("filter.options.nameDesc")}</option>
+        <option value="date-asc">{t("filter.options.dateAsc")}</option>
+        <option value="date-desc">{t("filter.options.dateDesc")}</option>
       </select>
     </div>
   );
--- client/src/pages/Hiking.tsx
+++ client/src/pages/Hiking.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useOutletContext } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import styles from "../styles/Hiking.module.scss";
